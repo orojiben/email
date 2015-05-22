e214dc284650283ffe8a9cf61feb60733833ae09
orojiben <orojiben@gmail.com>
2015-05-22 13:27:44 +0700

    t
    t

--- email.js
+++ email.js
@@ -1,4 +1,36 @@
-﻿var nodemailer = require("nodemailer");
+﻿var express=require('express');
+var nodemailer = require("nodemailer");
+var app=express();
+
+/*------------------Routing Started ------------------------*/
+
+app.get('/',function(req,res){
+res.sendfile('index.html');
+});
+app.get('/send',function(req,res){
+	
+	smtpTransport.sendMail({// sender address
+	   to: "Your Name <"+req.query.user_r+">", // comma separated list of receivers
+	   subject: "Welcome to www.nkaujhmono.com", // Subject line
+	   text: "Link for Login http://www.nkaujhmono.com/ok?x="+req.query.x+"&user="+req.query.user_r // plaintext body
+	}, function(error, response){
+	   if(error){
+		   console.log(error);
+	   }else{
+		   console.log("Message sent: " + response);
+		   res.end("ok_ok");
+	   }
+	});
+	
+});
+
+/*--------------------Routing Over----------------------------*/
+
+app.listen(3001,function(){
+console.log("Express Started on Port 3000");
+});
+
+//var nodemailer = require("nodemailer");
 
 var smtpTransport = nodemailer.createTransport("SMTP",{
    service: "Gmail",
@@ -7,7 +39,7 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
        pass: "nutsuchiraruwa01#"
    }
 });
-
+/*
 x = 1;
 user_r = 1;
 
@@ -26,4 +58,5 @@ function sendMail_()
 	});
 }
 
-sendMail_();
+
+sendMail_();*/
